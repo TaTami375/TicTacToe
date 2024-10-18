37477d50359d162ebaa7eb822cd66cf8a20d5c35
Maksim <makskupriyanoff@yandex.ru>
2024-10-18 23:12:35 +0300

    Добавьте файлы проекта.

---
+++ TicTacToe/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox11_Click(object sender, EventArgs e)
+        {
+            using (MemoryStream ms = new MemoryStream(TicTacToe.Properties.Resources.Tic))
+            {
+                pictureBox11.Image = Image.FromStream(ms);
+            }
+            pictureBox11.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void pictureBox21_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox31_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox22_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox32_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox23_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox33_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
